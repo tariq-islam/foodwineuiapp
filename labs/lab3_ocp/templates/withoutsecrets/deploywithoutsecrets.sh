73321fd4262de5d1905bcc4947d71e715b342ddc
Tariq Islam <tislam@redhat.com>
2017-04-26 17:12:18 -0400

    fixing templates for deployment

---
+++ labs/lab3_ocp/templates/withoutsecrets/deploywithoutsecrets.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+oc create -f postgres-service.yaml                                    
+oc create -f postgres-pod.yaml                                        
+oc create -f food-service.yaml                                        
+oc create -f food-service-pod.yaml                                    
+oc create -f wine-service.yaml                                        
+oc create -f wine-service-pod.yaml                                    
+oc create -f ui-service.yaml
+oc create -f ui-pod.yaml
